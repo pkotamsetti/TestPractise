dcf0eac011aa56052cd29846d968946772cdc10b
Prasad Kotamsetti <pkotamsetti@gmail.com>
2019-12-08 12:16:25 +0000

    testing CHange
    testing CHange

--- Features/Login.feature
+++ Features/Login.feature
@@ -4,13 +4,9 @@ Scenario: Asos Registration
 
 Given The new user is on homepage
 When User enters the valid details
-|email|kotamsetti72@gmail.com|
-|firstname|James|
-|lastname|Anderson|
-|password|Testing2017|
-|DOB_D|15|
-|DOB_M|June|
-|DOB_Y|82|
+
+|email|password|firstname|lastname|dob_d|dob_m|dob_y|
+|kotamsetti72@gmail.com|James|Anderson|Testing|15|june|82|
 
 
 Then User must be registered
--- src/test/java/stepDef/StartStepDef.java
+++ src/test/java/stepDef/StartStepDef.java
@@ -1,8 +1,9 @@
 package stepDef;
 
 import java.io.File;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -13,6 +14,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
@@ -21,10 +23,13 @@ import cucumber.api.java.en.When;
 
 public class StartStepDef {
 
+	WebDriver driver;
+
 	By CookiesAcceptButton = By.cssSelector("a.cc-btn.cc-dismiss");
 	By escape = By.cssSelector(".content-wrapper");
 	By logRegislink = By.cssSelector("#myAccountDropdown > button > span > svg");
 	By regisElement = By.cssSelector("a._1k1reGo[href*='https://my.asos.com/identity']");
+	By emailele = By.xpath("//*[@id=\"Email\"]");
 
 	@Given("^The new user is on homepage$")
 	public void the_new_user_is_on_homepage() throws Throwable {
@@ -33,30 +38,36 @@ public class StartStepDef {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-infobars");
 		WebDriver driver = new ChromeDriver(options);
-		driver.get("https://www.asos.com/");
+//		driver.get("https://www.asos.com/");
+		driver.get(
+				"https://my.asos.com/identity/register?lang=en-GB&store=COM&country=GB&keyStoreDataversion=ckp5egq-11&returnUrl=https%3A%2F%2Fwww.asos.com%2F");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.MILLISECONDS);
-
-		WebElement target = driver.findElement(logRegislink);
-		Actions action = new Actions(driver);
-		action.moveToElement(target).build().perform();
-
-		WebDriverWait wait = new WebDriverWait(driver, 20);
-		WebElement regisLink = wait.until(ExpectedConditions.elementToBeClickable(regisElement));
-		regisLink.click();
+		
+//
+//		WebElement target = driver.findElement(logRegislink);
+//		Actions action = new Actions(driver);
+//		action.moveToElement(target).build().perform();
+//
+//		WebDriverWait wait = new WebDriverWait(driver, 20);
+//		WebElement regisLink = wait.until(ExpectedConditions.elementToBeClickable(regisElement));
+//		regisLink.click();
 //	    Added a method for screenshot 
-		File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(screenshotFile, new File("D:\\Screenshot.jpg"));
+//		File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+//		FileUtils.copyFile(screenshotFile, new File("D:\\Screenshot.jpg"));
 
 	}
 
 	@When("^User enters the valid details$")
-	public void user_enters_the_valid_details(DataTable arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		// For automatic transformation, change DataTable to one of
-		// List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-		// E,K,V must be a scalar (String, Integer, Date, enum etc)
-
+	public void user_enters_the_valid_details(DataTable userData) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+		
+		List<Map<String,String>> data = userData.asMaps(String.class,String.class);
+		driver.findElement(emailele).sendKeys(data.get(0).get("Email"));
+	    
 	}
 
 	@Then("^User must be registered$")
